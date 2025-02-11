3eeb51e0b77a026c55c908283d8bc51a640b52fc
kota_shiratori <swan.kouta@gmail.com>
2025-02-11 20:03:25 +0900

    ⚡️

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/user', (req, res) => {
+    res.send('ユーザーです。')
+})
+
+app.get('/user/info', (req, res) => {
+    res.send('ユーザー情報です。')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
